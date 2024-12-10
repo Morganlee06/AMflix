1c179b6ff328c788f04b75c9ba357b0aeec86375
Ausar-re17 <148381186+Ausar-re17@users.noreply.github.com>
2024-12-10 12:37:26 +0000

    tittel

---
+++ AMflix/AMflix/Data/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using AMflix.Models;
+
+namespace AMflix.Data
+{
+    public class SeedData
+    {
+        public static void SeedMovies(ApplicationDbContext context)
+        {
+            if(!context.Movies.Any())
+            {
+                var Movies = new List<Movies>
+                {
+                    new Movies
+                    {
+                     Title = "Morgan And The Seven Seas",
+                     Description = "Morgan is a powerful Caribbean priest, " +
+                     "deeply connected to the ancient spiritual traditions of " +
+                     "Vodou, Santería, and other Afro-Caribbean religions."
+
+                    },
+                };
+            }
+        }
+
+    }
+}
